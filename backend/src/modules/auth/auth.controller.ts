d097ee5323531039f632286d55250861befd986b
Fernanda Alice <113216913+feralice@users.noreply.github.com>
2024-05-28 22:07:42 -0400

    configurando vercel

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,9 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Public } from 'src/decorators/auth.decorator';
 import { AuthService } from './auth.service';
 import { LoginResponse } from './dto/login-response.dto';
 import { LoginDto } from './dto/login.dto';
-import { Public } from 'src/decorators/auth.decorator';
 
 @Controller()
 @ApiTags('Auth')
@@ -16,5 +16,11 @@ export class AuthController {
   @ApiOperation({ summary: 'Login' })
   login(@Body() { email, password }: LoginDto) {
     return this.authService.login(email, password);
+  }
+
+  @Get('hello')
+  @Public()
+  getHello() {
+    return 'Hello';
   }
 }
