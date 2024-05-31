e72ac2c8ba77435149ef358c39fc13cde8229f91
Fernanda Alice <113216913+feralice@users.noreply.github.com>
2024-05-30 23:09:10 -0400

    usando a api deployada

--- frontend/src/services/config.ts
+++ frontend/src/services/config.ts
@@ -3,6 +3,8 @@ import axios from "axios";
 const TOKEN_KEY = "accessToken";
 
 export const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: "https://ufam-explore.vercel.app/",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
-
