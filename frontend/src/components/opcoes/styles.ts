669ec0e28bb10292a6298194928a8b4edc7d18f2
Sarah Júlia <129016417+sarahjl1909@users.noreply.github.com>
2024-06-01 22:25:41 -0300

    adicionando arquivos do menu popup

---
+++ frontend/src/components/opcoes/styles.ts
@@ -0,0 +1,30 @@
+import { StyleSheet } from "react-native";
+
+
+export const styles = StyleSheet.create({
+  popup: {
+    borderRadius: 8,
+    borderColor: "darkblue",
+    borderWidth: 1,
+    backgroundColor: "rgba(255, 255, 255, 0.6)",
+    paddingHorizontal: 10,
+    position: 'absolute',
+    top: 106,
+    right: 55,
+  },
+  option:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 7,
+    borderBottomColor: '#ccc',
+  },
+
+  texto: {
+    color: "darkblue"
+  },
+
+});
+
+
+
