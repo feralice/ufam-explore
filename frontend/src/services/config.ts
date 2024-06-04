b46d4dc17e18bdae01e1765d2a478a8f5562a491
Fernanda Alice <113216913+feralice@users.noreply.github.com>
2024-06-03 22:41:33 -0400

    teste

--- frontend/src/services/config.ts
+++ frontend/src/services/config.ts
@@ -4,9 +4,6 @@ const TOKEN_KEY = "accessToken";
 
 export const api = axios.create({
   baseURL: "https://ufam-explore.vercel.app/",
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
 
 console.log("API", api);
