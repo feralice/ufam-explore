54732e7120d622553cc98706dd7124b68a0a6f58
Fernanda Alice <113216913+feralice@users.noreply.github.com>
2024-06-03 22:06:39 -0400

    tesst

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,28 +3,18 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
   });
 
   if (process.env.ENABLE_SWAGGER === 'true') {
-    const config = new DocumentBuilder()
-      .setTitle('Documentação com Swagger - Ufam Explore')
-      .setDescription(
-        'Documentação da API do projeto Ufam Explore, aplicação desenvolvida para a disciplina de Prática em engenharia de software na UFAM',
-      )
-      .setVersion('1.0')
-      .build();
+    const config = new DocumentBuilder().setVersion('1.0').build();
 
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, document);
   }
 
   await app.listen(process.env.PORT || 3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
-
 bootstrap();
