9b782d2142f61e9ce834ab9c31fac8160f8a3876
Fernanda Alice <113216913+feralice@users.noreply.github.com>
2024-06-21 18:35:03 -0400

    nova api

--- frontend/src/services/config.ts
+++ frontend/src/services/config.ts
@@ -2,7 +2,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://192.168.5.181:3000",
+  baseURL: "https://ufam-explore.vercel.app/",
 });
 
 api.interceptors.request.use(
