73b8d7d64865ec63f655d7dc3b133eb5537c58c8
Nilton <nilton.nascimento@icomp.ufam.edu.br>
2024-07-02 20:15:49 -0300

    [FRONT] Edit-Information-User

--- frontend/src/pages/edit-profile-informations/index.tsx
+++ frontend/src/pages/edit-profile-informations/index.tsx
@@ -9,6 +9,8 @@ import { IStore } from "../../store";
 import { styles } from "./style";
 import { useState } from "react";
 import { BlueButton } from "../../components/blue-button";
+import { cursos } from "../../utils/courses";
+import SelectDropdown from "react-native-select-dropdown";
 
 export const EditProfileInformation = () => {
   const user = useSelector((state: IStore) => state.user.user);
@@ -17,6 +19,7 @@ export const EditProfileInformation = () => {
     // Implementar a lógica para alterar a foto de perfil aqui
     console.log("Alterar foto de perfil");
   };
+  const [course, setCourse] = useState("");
   const [name, setName] = useState("OIIIIIIIIIIIIII");
 
   return (
@@ -49,7 +52,7 @@ export const EditProfileInformation = () => {
             testID="Name"
           />
         </View>
-        <Text style={styles.texto}>NOME DE USUÁRIO</Text>
+        <Text style={[styles.texto, { paddingTop: 20 }]}>NOME DE USUÁRIO</Text>
         <View style={[styles.texto, styles.BoxInput]}>
           <TextInput
             defaultValue={user.username}
@@ -57,15 +60,56 @@ export const EditProfileInformation = () => {
             testID="NameUser"
           />
         </View>
-        <Text style={styles.texto}>E-MAIL</Text>
-        <Text style={styles.textoAlternativo}>{user.email}</Text>
+        <Text style={[styles.texto, { paddingTop: 20 }]}>E-MAIL</Text>
+
+        <View style={[styles.texto, styles.BoxInput]}>
+          <TextInput
+            defaultValue={user.email}
+            onChangeText={setName}
+            testID="EmailUser"
+          />
+        </View>
         {user.perfilId === 1 && (
           <>
-            <Text style={styles.texto}>CURSO</Text>
-            <Text style={styles.textoAlternativo}>{user.curso}</Text>
+            <Text style={[styles.texto, { paddingTop: 20 }]}>CURSO</Text>
+            <View style={styles.boxInput}>
+              <SelectDropdown
+                data={cursos}
+                onSelect={(selectedItem, index) => {
+                  setCourse(selectedItem);
+                }}
+                renderButton={(selectedItem, isOpened) => {
+                  return (
+                    <View style={styles.dropdownButtonStyle}>
+                      <Text style={styles.dropdownButtonTxtStyle}>
+                        {selectedItem ? selectedItem : "Modifique seu curso"}
+                      </Text>
+                      <MaterialCommunityIcons
+                        name={isOpened ? "chevron-up" : "chevron-down"}
+                        style={styles.dropdownButtonArrowStyle}
+                      />
+                    </View>
+                  );
+                }}
+                renderItem={(item, index, isSelected) => {
+                  return (
+                    <View
+                      style={{
+                        ...styles.dropdownItemStyle,
+                        ...(isSelected && { backgroundColor: "#D2D9DF" }),
+                      }}
+                    >
+                      <Text style={styles.dropdownItemTxtStyle}>{item}</Text>
+                    </View>
+                  );
+                }}
+                dropdownStyle={styles.dropdownMenuStyle}
+              />
+            </View>
           </>
         )}
       </View>
+
       <BlueButton
         onPress={function (): void {
           Alert.alert("OMG this is a button that go to main menu");
