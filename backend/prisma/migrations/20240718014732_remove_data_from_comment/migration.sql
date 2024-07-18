e1cb6ae6f000c200d2103d101814ee5fb6badaab
feralice <fernandaalice2003@gmail.com>
2024-07-17 21:57:50 -0400

    ajustes backend criar comentario

---
+++ backend/prisma/migrations/20240718014732_remove_data_from_comment/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `data` on the `Comentario` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Comentario" DROP COLUMN "data";
